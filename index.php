0924b707327e3bbe15db9d420d1ebe913ed63127
Tapas Kumar Mishra <tap1cse@gmail.com>
2019-09-24 18:53:38 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Tapas Kumar Mishra's Home</title>
+        <meta name="description" content="An interactive getting started guide for Brackets.">
+        <link rel="stylesheet" href="main.css">
+        <script src="script/jquery-1.11.2.min.js"></script>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+    </head>
+    <body>
+        <!--<div id="nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Academic</a></li>
+                <li><a href="#">Programming</a></li>
+                <li><a href="#">Random</a></li>
+                <li><a href="#">J&amp;C</a></li>
+                <li><a href="#">People I Follow</a></li>
+
+            </ul>
+        </div>-->
+        <div id="content">
+            <div class="left">
+                <img alt="Tapas Kumar Mishra" src="images/3.jpg" />
+            </div>
+
+            <div class="right">
+                    <h3>Tapas Kumar Mishra</h3> 
+                    <h4> Ph.D Scholar</h4>
+                    <h4>Department of Computer Science and Engineering</h4>
+                    <h4>Indian Institute of Technology, Kharagpur</h4>  
+                    <br />                    
+            </div>
+        </div>
+       <hr />
+        <!--
+            MADE WITH <3 AND JAVASCRIPT
+        -->
+        <div style="clear:both">
+            <h3>Academic Interests</h3>
+            <div class="hr"><hr /></div>
+            
+            <ul>
+                <li><a href="http://en.wikipedia.org/wiki/Combinatorics">Combinatorics</a></li>
+                <li><a href="http://en.wikipedia.org/wiki/Hypergraph">Graph and Hypergraph Coloring</a></li>
+                <li><a href="http://en.wikipedia.org/wiki/Randomized_algorithm">Randomized Algorithms</a></li>
+                <li><a href="http://en.wikipedia.org/wiki/Ramsey_theory">Ramsey Theory</a></li>
+                <li><a href="http://en.wikipedia.org/wiki/Approximation_algorithm">Approximation Algorithms</a></li>
+                <li><a href="http://en.wikipedia.org/wiki/Computational_geometry">Linear Algebraic applications to Combinatorics</a></li>
+            </ul>
+        </div>
+        
+        <div>
+            <h3>Publications and Preprints</h3>
+            <div class="hr"><hr /></div>
+            <ul>
+                <li>With Niranjan Balachandran, Rogers Mathew, Sudebkumar Prasant Pal. Induced bisecting families for hypergraphs. <a href="https://arxiv.org/abs/1610.00140"> Preprint: arXiv:1610.00140.</a> 2016.</li>
+                <li>With Niranjan Balachandran, Rogers Mathew, Sudebkumar Prasant Pal. Bisecting families for set systems. <a href="https://arxiv.org/abs/1604.01482"> Preprint: arXiv:1604.01482.</a> 2016.</li>
+                <li>With Sudebkumar Prasant Pal. Bicoloring covers for graphs and hypergraphs. <a href="http://arxiv.org/abs/1501.00343">Preprint: arXiv:1501.00343.</a> 2015.</li>
+                <li>With Sudebkumar Prasant Pal. <a href="http://jgaa.info/getPaper?id=311"> Lower bounds for Ramsey numbers for complete bipartite and 3-uniform tripartite subgraphs</a> Journal of Graph Algorithms and Applications, Vol. 17, no. 6, pp. 671-688, 2013.</li>
+                <li>With Sudebkumar Prasant Pal.<a href="http://link.springer.com/chapter/10.1007/978-3-642-36065-7_24"> Lower Bounds for Ramsey Numbers for Complete Bipartite and 3-Uniform Tripartite Subgraphs</a> WALCOM: Algorithms and Computation
+Lecture Notes in Computer Science Volume 7748, 2013, pp 257-264.</li>
+            </ul>
+        
+        </div>
+        
+        <div>
+            <h3>Projects and Seminars</h3>
+            <div class="hr"><hr /></div>
+            
+            <ul>
+                <li><a href="files/11CS60R32_TapasKumarMishra_Thesis.pdf"> M.Tech. Thesis: Bounds on Ramsey numbers for
+complete bipartite and 3-uniform tripartite subgraphs</a></li>
+                <li><a href="files/ProjectPresentation.pdf"> M.Tech. Project Seminar: Bounds on Ramsey numbers for
+complete bipartite and 3-uniform tripartite subgraphs</a></li>
+                <li><a href="files/Ramsey_seminar.pdf"> An Introduction to Ramsey Theory</a></li>
+                <li><a href="files/SEMANTIC%20WEB%20report.pdf"> Report: An Introduction to semantic web</a></li>
+                <li><a href="files/Semantic%20web_me.pdf"> Seminar: An Introduction to semantic web</a></li>
+                <li><a href="files/Deliverables.tar"> A mips64 implementation in verilog</a></li>
+            </ul>
+        </div>
+        <!--
+            WHAT IS BRACKETS?
+        -->
+    <div>
+           <h3>Educational Qualification</h3>
+            <div class="hr"><hr /></div>
+        <ul>
+                <li>Ph.D.(Continuing): Computer Science and Engg., Indian Institute of Technology, Kharagpur. (2013-)</a></li>
+                <li>M.Tech: Computer Science and Engg., Indian Institute of Technology, Kharagpur. (2011-2013)</li>
+                <li>B.Tech: Computer Science and Engg., Veer Surendra Sai University of Technology (formerly UCE, Burla), Burla. (2006-2010)</li>
+                <li>12th: GM Junior College, Sambalpur.(2004-2006)
+                <li>Upto 10th: S.A.I.I.E. &amp; R.,Sambalpur.</li>
+                <li><a href="files/Tapas_2015.pdf">Resume</a></li>
+            </ul>
+    
+        </div>
+        <!--  Me on Web    -->
+          
+        <div class="hr"><hr /></div>
+        <div id="container">
+           
+            
+            <div id="widget2" class="quora"> <input type='button' value='Quora' class="btn" onclick="document.location.href='http://www.quora.com/Tapas-Kumar-Mishra';"/></div>
+            <div id="widget4"><input type='button' value='Google Scholar' class="btn" onclick="document.location.href='http://scholar.google.com/citations?user=t-EOWeEAAAAJ&hl=en';"/></div>
+            <div id="widget4"><input type='button' value='DPLB' class="btn" onclick="document.location.href='http://dblp.uni-trier.de/pers/hd/m/Mishra_0001:Tapas_Kumar';"/></div>
+            <div id="widget3"><a href="https://twitter.com/tap1cse" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @tap1cse</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
+            <div class="g-follow" data-annotation="bubble" data-height="24" data-href="//plus.google.com/u/0/106937619902914158348" data-rel="author"></div>
+            <div></div>
+        </div>
+        <hr />
+    </body>
+</html>
+<!--
+
+    [[[[[[[[[[[[[[[     ]]]]]]]]]]]]]]]
+    [::::::::::::::     ::::::::::::::]
+    [::::::::::::::     ::::::::::::::]
+    [::::::[[[[[[[:     :]]]]]]]::::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[    CODE THE WEB     ]:::::]
+    [:::::[  http://brackets.io ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [:::::[                     ]:::::]
+    [::::::[[[[[[[:     :]]]]]]]::::::]
+    [::::::::::::::     ::::::::::::::]
+    [::::::::::::::     ::::::::::::::]
+    [[[[[[[[[[[[[[[     ]]]]]]]]]]]]]]]
+
+-->
